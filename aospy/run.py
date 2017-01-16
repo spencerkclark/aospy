030f3552f2692a1415f6d988d7c7e5fbdbe90a6e
Spencer Clark <skclark@princeton.edu>
2017-01-16 10:55:22 -0500

    MAINT Remove _set_direc method from run.py
    This is another method that is part of the temporarily-removed 'gfdl_repo' method of loading files

--- aospy/run.py
+++ aospy/run.py
@@ -2,15 +2,6 @@
 from .utils.times import datetime_or_default
 
 
-def _set_direc(data_direc, ens_mem_prefix, ens_mem_ext,
-               ens_mem_suffix):
-    """Set the list of paths containing the Run's netCDF data."""
-    if all((ens_mem_prefix, ens_mem_ext, ens_mem_suffix)):
-        return [ens_mem_prefix + ext + ens_mem_suffix
-                for ext in ens_mem_ext]
-    return data_direc
-
-
 class Run(object):
     """Model run parameters."""
     def __init__(self, name=None, description=None, proj=None,
